eb328a930088fb1a3ac5c26bc775c30dabf4d273
Vladimir12071983 <vgura69@gmail.com>
2019-09-16 21:39:21 +0300

    add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 const gulp = require ('gulp');
+const babel = require ('gulp-babel');
 // Зависимости Gulp будут здесь
 
 gulp.task('default', function () {
 	// Задачи Gulp будут здесь
-	
-})
+
+	// исходный код для Node
+	gulp.src('es6/**/*.js')
+	.pipe(babel())
+	.pipe(gulp.dest('dist'));
+
+	// исходный код для браузера
+	gulp.src('public/es6/**/*.js')
+	.pipe(babel())
+	.pipe(gulp.dest('public/dist'));
+});
+
