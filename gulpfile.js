7f2316c3e433ec20b5bef58ad0359c435309de4f
Vladimir12071983 <vgura69@gmail.com>
2019-09-17 14:27:44 +0300

    eslint work

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 const gulp = require ('gulp');
 const babel = require ('gulp-babel');
+const eslint = require ('gulp-eslint');
 // Зависимости Gulp будут здесь
+
+
+exports.default = asyncAwaitTask;
+
 
 gulp.task('default', function () {
 	// Задачи Gulp будут здесь
+
+	// запуск Eslint
+	gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+	.pipe(eslint())
+	.pipe(eslint.format());
 
 	// исходный код для Node
 	gulp.src('es6/**/*.js')
@@ -16,3 +26,10 @@ gulp.task('default', function () {
 	.pipe(gulp.dest('public/dist'));
 });
 
+const fs = require('fs');
+
+async function asyncAwaitTask() {
+  const { version } = fs.readFileSync('package.json');
+  console.log(version);
+  await Promise.resolve('some result');
+}
